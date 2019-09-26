4a58bb6013f193b72831e899cfae0fbc47ef6e19
Andracarys <andracarys@users.noreply.github.com>
2019-09-26 12:36:06 -0400

    update

--- hackathon/pagweb/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Freelancer
-from .models import Profesion
-
-admin.site.register(Freelancer)
-admin.site.register(Profesion)
--- hackathon/pagweb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagwebConfig(AppConfig):
-    name = 'pagweb'
